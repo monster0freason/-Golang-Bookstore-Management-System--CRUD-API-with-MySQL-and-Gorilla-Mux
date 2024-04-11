acb6299774765ce21c66b9eb6991d2d0e5a9e5e5
Ankit bhardwaj <101131763+monster0freason@users.noreply.github.com>
2024-04-11 14:20:48 +0530

    added tests

---
+++ cmd/main/server_test.go
@@ -0,0 +1,103 @@
+package main
+
+import (
+    "fmt"
+    "net/http"
+    "net/http/httptest"
+    "testing"
+    "github.com/stretchr/testify/assert"
+    "github.com/monster0freason/golang-bookstore-management-system/pkg/controllers"
+	"github.com/monster0freason/golang-bookstore-management-system/pkg/models"
+    
+    "encoding/json"
+    "io"
+	"strings"
+)
+
+func TestGetBooks1(t *testing.T) {
+    testServer := httptest.NewServer(http.HandlerFunc(controllers.GetBooks))
+    defer testServer.Close()
+
+    testClient := testServer.Client()
+    fmt.Println((testServer.URL))
+    testClient.Get(testServer.URL)
+
+    response, err := testClient.Get(testServer.URL)
+    if err != nil {
+        t.Error(err)
+    }
+
+    // Read the response body
+    body, err := io.ReadAll(response.Body)
+    if err != nil {
+        t.Error(err)
+    }
+
+    // Assert that the HTTP status code is equal to http.StatusOK (200)
+    assert.Equal(t, http.StatusOK, response.StatusCode)
+
+    // Add assertions for the response body
+    // For example, you can assert that the body contains certain data or has a specific format
+    // For now, let's just assert that the body is not empty
+    assert.NotEmpty(t, body)
+}
+
+
+func TestCreateBook(t *testing.T) {
+    testServer := httptest.NewServer(http.HandlerFunc(controllers.CreateBook))
+    defer testServer.Close()
+
+    testClient := testServer.Client()
+    fmt.Println(testServer.URL)
+
+    // Create a new book object to be sent in the request body
+    newBook := models.Book{
+        Name:        "Test Book",
+        Author:      "Test Author",
+        Publication: "Test Publisher",
+    }
+
+    // Convert the book object to JSON format
+    reqBody, err := json.Marshal(newBook)
+    if err != nil {
+        t.Error(err)
+    }
+
+    // Create a POST request with the JSON request body
+    req, err := http.NewRequest("POST", testServer.URL, strings.NewReader(string(reqBody)))
+    if err != nil {
+        t.Error(err)
+    }
+    req.Header.Set("Content-Type", "application/json")
+
+    // Send the POST request
+    response, err := testClient.Do(req)
+    if err != nil {
+        t.Error(err)
+    }
+    defer response.Body.Close()
+
+    // Assert that the HTTP status code is equal to http.StatusCreated (201)
+    assert.Equal(t, http.StatusCreated, response.StatusCode)
+
+    // Read the response body
+    body, err := io.ReadAll(response.Body)
+    if err != nil {
+        t.Error(err)
+    }
+
+    // Parse the response body into a map
+    var responseBody map[string]interface{}
+    err = json.Unmarshal(body, &responseBody)
+    if err != nil {
+        t.Error(err)
+    }
+
+    // Assert that the response body contains the created book data
+    assert.NotNil(t, responseBody["ID"])
+    assert.Equal(t, newBook.Name, responseBody["name"])
+    assert.Equal(t, newBook.Author, responseBody["author"])
+    assert.Equal(t, newBook.Publication, responseBody["publication"])
+}
+
+
--- go.mod
+++ go.mod
@@ -3,12 +3,17 @@ module github.com/monster0freason/golang-bookstore-management-system
 go 1.22.0
 
 require (
+	github.com/gorilla/mux v1.8.1
+	github.com/jinzhu/gorm v1.9.16
+	github.com/stretchr/testify v1.9.0
+)
+
+require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/go-sql-driver/mysql v1.5.0 // indirect
-	github.com/gorilla/mux v1.8.1 // indirect
-	github.com/jinzhu/gorm v1.9.16 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/mattn/go-sqlite3 v1.14.22 // indirect
-	gorm.io/driver/sqlite v1.5.5 // indirect
-	gorm.io/gorm v1.25.7 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
